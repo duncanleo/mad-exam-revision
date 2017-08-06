8f8491ee27fda48bda698d42cbbe0ed734e1ea70
Duncan Leo <duncanleo97@gmail.com>
2017-08-06 12:17:09 +0800

    add Location distance calculation

--- app/src/main/java/me/duncanleo/examrevision/MainActivity.java
+++ app/src/main/java/me/duncanleo/examrevision/MainActivity.java
@@ -1,13 +1,18 @@
 package me.duncanleo.examrevision;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     public static final String KEY_EDIT_TEXT = "editText";
 
     private EditText editText;
+    private TextView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = (EditText)findViewById(R.id.editText);
+        location = (TextView)findViewById(R.id.locationTextView);
+
+        Location np = new Location("dummyprovider");
+        np.setLatitude(1.333463);
+        np.setLongitude(103.776764);
+
+        Location sp = new Location("dummyprovider");
+        sp.setLatitude(1.308358);
+        sp.setLongitude(103.779141);
+
+        location.setText(String.format(
+                Locale.getDefault(),
+                "The distance between NP and SP is %.2fm",
+                np.distanceTo(sp)
+        ));
     }
 
     @Override
